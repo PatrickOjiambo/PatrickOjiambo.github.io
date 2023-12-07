8c00744bce239ef22e294ddea602ce9c14432e80
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-12-07 21:07:43 +0300

    feat: Finish UI for shopElegance landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  base: '/',
+  // base: '/',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+       
+        Poppins: ['Poppins', "sans-serif"],
+        Whisper: ['Whisper', "sans-serif"],
+      },
+      colors:{
+        funnyOrange: "#D1A206"
+      }
+    },
   },
   plugins: [],
 
